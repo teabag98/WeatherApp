efc1db762611b4b0c2b9a72f8962ae6e51798a64
teabag98 <eric.kingoo2014@gmail.com>
2023-03-15 08:10:26 +0300

    innitial commit

---
+++ lib/core/platform/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/services.dart';
+
+class AppColors {
+  static const sunny = Color(0xFF47AB2F);
+  static const cloudy = Color(0xFF54717A);
+  static const rainy = Color(0xFF57575D);
+}
